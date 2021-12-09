ec45d31032878e6349f6e39393a8598e011af62a
OzRagwort <ozragwort@gmail.com>
2021-12-09 14:42:58 +0900

    fix: 에러 메시지 enum에서 다른 상수의 값을 제대로 받아오도록 수정

--- src/main/java/vendingmachine/enums/ErrorMessage.java
+++ src/main/java/vendingmachine/enums/ErrorMessage.java
@@ -1,10 +1,12 @@
 package vendingmachine.enums;
 
 public enum ErrorMessage {
-	ERROR_MESSAGE("[ERROR]"),
-	AMOUNT_NUMBER_FORMAT_ERROR_MESSAGE(ERROR_MESSAGE + "금액은 숫자로 입력해주세요."),
-	AMOUNT_NEGATIVE_NUMBER_ERROR_MESSAGE(ERROR_MESSAGE + "0 이상의 금액을 입력해주세요."),
-	AMOUNT_NOT_DIVISIBLE_BY_10_ERROR_MESSAGE(ERROR_MESSAGE + "금액은 " + Coin.COIN_10 + "으로 나누어 떨어져야 합니다.");
+	ERROR_MESSAGE("[ERROR] "),
+	AMOUNT_NUMBER_FORMAT_ERROR_MESSAGE(ERROR_MESSAGE.get() + "금액은 숫자로 입력해주세요."),
+	AMOUNT_NEGATIVE_NUMBER_ERROR_MESSAGE(ERROR_MESSAGE.get() + "0 이상의 금액을 입력해주세요."),
+	AMOUNT_NOT_DIVISIBLE_BY_10_ERROR_MESSAGE(ERROR_MESSAGE.get() + "금액은 "
+		+ Coin.COIN_10.get()
+		+ "으로 나누어 떨어져야 합니다.");
 
 	private final String message;
 
