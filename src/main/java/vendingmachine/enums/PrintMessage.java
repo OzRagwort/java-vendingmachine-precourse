5c65c4dd77a03f6c77e75e6fc50530f44962da46
OzRagwort <ozragwort@gmail.com>
2021-12-09 20:50:42 +0900

    fix: 상수값 오타 수정

--- src/main/java/vendingmachine/enums/PrintMessage.java
+++ src/main/java/vendingmachine/enums/PrintMessage.java
@@ -5,7 +5,7 @@ public enum PrintMessage {
 	RESPONSE_COIN_STATE_MESSAGE("자판기가 보유한 동전"),
 	REQUEST_ADD_PRODUCT_MESSAGE("상품명과 가격, 수량을 입력해 주세요."),
 	REQUEST_MONEY_MESSAGE("투입 금액을 입력해 주세요."),
-	RESPONSE_IN_MONEY_FORM("투입 금액 : %d원"),
+	RESPONSE_IN_MONEY_FORM("투입 금액: %d원"),
 	REQUEST_PRODUCT_NAME_TO_BUY("구매할 상품명을 입력해 주세요.");
 
 	private final String message;
