d314185c6c37575e1e893b0226e861b38bdef723
OzRagwort <ozragwort@gmail.com>
2021-12-11 22:20:51 +0900

    fix(Price): 가격이 제대로 설정되도록 수정

--- src/main/java/vendingmachine/domain/Price.java
+++ src/main/java/vendingmachine/domain/Price.java
@@ -9,14 +9,15 @@ public class Price extends Amount {
 
 	public Price(String value) {
 		super(value);
-		validateMinimumPrice();
+		int price = Integer.parseInt(value);
+		validateMinimumPrice(price);
 	}
 
 	public int get() {
 		return price;
 	}
 
-	private void validateMinimumPrice() {
+	private void validateMinimumPrice(int price) {
 		if (price < MINIMUM_PRICE) {
 			throw new IllegalArgumentException(PRICE_LOWER_THEN_MINIMUM_PRICE_ERROR_MESSAGE.get());
 		}
