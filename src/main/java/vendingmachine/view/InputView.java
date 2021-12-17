684d058639384eb33bcc708ed4b34589cbbae30c
OzRagwort <ozragwort@gmail.com>
2021-12-17 16:57:08 +0900

    feat: 보유 금액을 입력하라는 출력 문구 추가

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -6,8 +6,12 @@ import vendingmachine.domain.Money;
 import vendingmachine.dto.RequestHoldingMoneyDto;
 
 public class InputView {
+	private static final String REQUEST_HOLDING_MONEY_MESSAGE = "자판기가 보유하고 있는 금액을 입력해 주세요.";
+
 	public static RequestHoldingMoneyDto inputHoldingMoney() {
+		System.out.println(REQUEST_HOLDING_MONEY_MESSAGE);
 		Money money = new Money(readLine());
+		System.out.println();
 		return new RequestHoldingMoneyDto(money);
 	}
 }
